5c782d2909a9bf1e631d4100db6faf6320f33a0f
李承斌 <15031617367@163.com>
2020-11-06 15:57:01 +0800

    服务器调试

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ const querystring = require('querystring');
 const server = http.createServer((request, response) => {
   let APilist = request.url;
   APilist = APilist.slice(0,APilist.indexOf("?"))
-
+  console.log("监听到访问了")
   if(APilist === "/baiduCurrency"){
+    console.log("进入了百度PI的判断条件")
     let {query} = url.parse(request.url,true);
     const querys = querystring.stringify({
       q: query.word,
